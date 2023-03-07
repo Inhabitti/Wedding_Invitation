cd68fc0daf893edb1572a093740835c8e24fb93a
OliviaBoinLee <125447175+OliviaBoinLee@users.noreply.github.com>
2023-03-07 21:06:24 +0900

    Update lightbox-plus-jquery.js

--- assets/javascript/lightbox-plus-jquery.js
+++ assets/javascript/lightbox-plus-jquery.js
@@ -11564,3 +11564,17 @@ return jQuery;
   return new Lightbox();
 }));
 
+function disableScroll() {
+  // Get the current page scroll position
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  var scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+  // if any scroll is attempted, set this to the previous value
+  window.onscroll = function() {
+      window.scrollTo(scrollLeft, scrollTop);
+  };
+}
+
+function enableScroll() {
+  window.onscroll = function() {};
+}
