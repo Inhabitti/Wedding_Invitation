3e259c4f29b60ea17191653b09f89365948fa148
OliviaBoinLee <125447175+OliviaBoinLee@users.noreply.github.com>
2023-03-07 17:38:05 +0900

    Update default.js

--- assets/javascript/default.js
+++ assets/javascript/default.js
@@ -23,43 +23,39 @@ document.getElementById('guestbook-form').addEventListener('submit', function(ev
 
 
 /* 방명록 로컬 스토리지*/
-// 방명록 입력폼과 메시지 리스트를 가져오기
-const guestbookForm = document.getElementById('guestbook-form');
-const guestbookInput = document.getElementById('guestbook-input');
-const guestbookMessages = document.getElementById('guestbook-messages');
+function addMessageToLocalStorage(message) {
+  // 로컬 스토리지에서 이전에 저장된 메시지들을 불러옵니다.
+  const messages = JSON.parse(localStorage.getItem('guestbookMessages')) || [];
 
-// 방명록 메시지를 localStorage에서 불러오기
-let guestbookData = localStorage.getItem('guestbookData');
-guestbookData = guestbookData ? JSON.parse(guestbookData) : [];
+  // 새로운 메시지 객체를 생성합니다.
+  const newMessage = {
+    id: Date.now(),
+    text: message,
+    timestamp: new Date().toISOString(),
+  };
 
-// 메시지 리스트를 출력하는 함수
-function renderMessages() {
-  guestbookMessages.innerHTML = '';
-  guestbookData.forEach((message) => {
+  // 새로운 메시지 객체를 로컬 스토리지의 메시지 배열에 추가합니다.
+  messages.push(newMessage);
+
+  // 로컬 스토리지에 메시지 배열을 저장합니다.
+  localStorage.setItem('guestbookMessages', JSON.stringify(messages));
+}
+function getMessagesFromLocalStorage() {
+  // 로컬 스토리지에서 이전에 저장된 메시지들을 불러옵니다.
+  const messages = JSON.parse(localStorage.getItem('guestbookMessages')) || [];
+
+  return messages;
+}
+window.addEventListener('load', () => {
+  const messages = getMessagesFromLocalStorage();
+
+  messages.forEach((message) => {
     const li = document.createElement('li');
-    li.textContent = message;
+    li.innerHTML = `${message.text} <span class="timestamp">${new Date(message.timestamp).toLocaleString()}</span>`;
     guestbookMessages.appendChild(li);
   });
-}
+});
 
-// 입력 폼 제출 시 실행되는 함수
-function handleSubmit(event) {
-  event.preventDefault();
-  const message = guestbookInput.value.trim();
-  if (!message) {
-    return;
-  }
-  guestbookData.push(message);
-  localStorage.setItem('guestbookData', JSON.stringify(guestbookData));
-  renderMessages();
-  guestbookInput.value = '';
-}
-
-// 페이지 로드 시 메시지 리스트 출력
-renderMessages();
-
-// 이벤트 리스너 등록
-guestbookForm.addEventListener('submit', handleSubmit);
 
 /*네이버 지도*/
 var HOME_PATH = window.HOME_PATH || '.';
