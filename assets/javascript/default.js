7f0b1c5ef689297689333b70fe98baee9e37f3cd
OliviaBoinLee <125447175+OliviaBoinLee@users.noreply.github.com>
2023-03-10 17:19:15 +0900

    Update default.js

--- assets/javascript/default.js
+++ assets/javascript/default.js
@@ -126,3 +126,28 @@ setTimeout(typing, 0); // 최초 딜레이 없이 타이핑 시작
 
 
 
+
+
+
+const h2 = document.querySelector('.blinking__txt');
+const cursor = document.querySelector('.cursor');
+
+const typing = function(_, counter = 0) {
+    // 출력할 내용
+  const txt = `Wedding Invitation`;
+  
+  setInterval(() => {
+    // 글자가 모두 출력되면 setInterval을 멈출 것
+    if (txt.length === counter) {
+      cursor.classList.add('blink_animate');
+      return;
+    };
+    // 문자열 하나하나 h2의 텍스트 컨텐츠로 추가한다 
+    h2.textContent += txt[counter];
+    // 카운터 증산
+    counter++;
+  }, 80); 
+};
+
+// 페이지 요소가 로드되면 타이핑 애니메이션을 수행할 것
+window.addEventListener('load', typing);
