74e39ce2edab31aabe8b077a9428013c2f265f72
OliviaBoinLee <125447175+OliviaBoinLee@users.noreply.github.com>
2023-03-10 18:12:56 +0900

    Update default.js

--- assets/javascript/default.js
+++ assets/javascript/default.js
@@ -101,3 +101,21 @@ naver.maps.Event.addListener(marker, "click", function(e) {
 
 
 
+
+
+
+
+
+
+
+
+const invitationTitle = document.getElementById("invitation-title");
+const text = "Emily & John's Wedding Invitation";
+let index = 0;
+
+setInterval(() => {
+  invitationTitle.innerHTML = text.slice(0, index);
+  index++;
+}, 100);
+
+
