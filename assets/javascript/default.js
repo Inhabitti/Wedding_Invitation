6bdc4b364bc6f2685c1730bc0f12093d0af0da86
OliviaBoinLee <125447175+OliviaBoinLee@users.noreply.github.com>
2023-03-29 13:24:34 +0900

    Update default.js

--- assets/javascript/default.js
+++ assets/javascript/default.js
@@ -13,35 +13,13 @@ var WeddingHall = new naver.maps.LatLng(36.3456884, 127.3545292),
         position: WeddingHall
     });
 
-var contentString = [
-    '<div class="iw_inner">',
-    '   <h3>에스가든웨딩</h3>',
-    '   <p>대전광역시 서구</br>월드컵대로484번안길 10</br>',
-    '       <a href="https://naver.me/FQaRiSBj" target="_blank">네이버 지도에서 보기</a>',
-    '   </p>',
-    '</div>'
-].join('');
-
-var infowindow = new naver.maps.InfoWindow({
-    content: contentString,
-    maxWidth: 250,
-    backgroundColor: "#ffffff",
-    borderColor: "#010101",
-    borderWidth: 1,
-    anchorSize: new naver.maps.Size(30, 30),
-    anchorSkew: true,
-    anchorColor: "#eee",
-    pixelOffset: new naver.maps.Point(20, -20)
+var marker = new naver.maps.Marker({
+  map: map,
+  position: WeddingHall,
+  iconImage: {
+    size: new naver.maps.Size(30, 30)
+  }
 });
-
-naver.maps.Event.addListener(marker, "click", function(e) {
-    if (infowindow.getMap()) {
-        infowindow.close();
-    } else {
-        infowindow.open(map, marker);
-    }
-});
-
 
 var target = document.querySelectorAll('.btn_open');
 var btnPopClose = document.querySelectorAll('.pop_wrap .btn_close');
