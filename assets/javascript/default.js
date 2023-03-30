b5479b7b24e77c796c8e4b658146da74c9ed2d9e
OliviaBoinLee <125447175+OliviaBoinLee@users.noreply.github.com>
2023-03-30 19:38:54 +0900

    Update default.js

--- assets/javascript/default.js
+++ assets/javascript/default.js
@@ -22,3 +22,40 @@ var marker = new naver.maps.Marker({
 });
 
 
+// Get the modal
+var modal1 = document.getElementById("modal1");
+var modal2 = document.getElementById("modal2");
+
+// Get the button that opens the modal
+var btn1 = document.querySelector(".btn1");
+var btn2 = document.querySelector(".btn2");
+
+// Get the <span> element that closes the modal
+var close1 = document.querySelector("#modal1 .close");
+var close2 = document.querySelector("#modal2 .close");
+
+// When the user clicks the button, open the modal 
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+close1.onclick = function() {
+  modal1.style.display = "none";
+}
+close2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
