e88bc0c9eb669abd42729105ae28cf52a5bab99b
OliviaBoinLee <125447175+OliviaBoinLee@users.noreply.github.com>
2023-04-06 00:02:17 +0900

    Update default.js

--- assets/javascript/default.js
+++ assets/javascript/default.js
@@ -6,7 +6,9 @@ var HOME_PATH = window.HOME_PATH || '.';
 var WeddingHall = new naver.maps.LatLng(36.3456884, 127.3545292),
     map = new naver.maps.Map('map', {
         center: WeddingHall.destinationPoint(0, 500),
-        zoom: 15
+        zoom: 15,
+        logoControl: false,
+        scaleControl: false
     }),
     marker = new naver.maps.Marker({
         map: map,
@@ -19,6 +21,9 @@ var marker = new naver.maps.Marker({
   iconImage: {
     size: new naver.maps.Size(30, 30)
   }
+});
+var zoomControl = new naver.maps.ZoomControl({
+    position: naver.maps.Position.TOP_LEFT
 });
 
 
